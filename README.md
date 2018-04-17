2cc4bd7e5cbb0b8a02ebe693ef4335578a9a1ca1
jediah6 <jediah6@cs.washington.edu>
2018-04-17 13:12:06 -0700

    minor changes

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # User Manual
 Smerge is a tool used for automating and minimizing user input in merge conflict resolutions.
 ## Installation
-* Clone the Smerge repository to ~/.
+* Clone the *smerge* repository to ~/.
 * Update your `.gitconfig` to include: 
 ```bash
-    [mergetool "smerge"]
-                cmd = java -jar ~/smerge/Merger.jar \$BASE \$LOCAL \$REMOTE \$MERGED
-    [merge]
+[mergetool "smerge"]
+        cmd = java -jar ~/smerge/Merger.jar \$BASE \$LOCAL \$REMOTE \$MERGED
+[merge]
         tool = smerge
 ```
 
 ## Usage
+You may run *smerge* as a [git mergetool](https://git-scm.com/docs/git-mergetool) through the following command:
+
 `git mergetool --tool=smerge <conflicting file>`
+
 
 ## Options
 
--- src/main/java/smerge/Merger.java
+++ src/main/java/smerge/Merger.java
@@ -2,15 +2,11 @@ package smerge;
 
 import java.io.FileNotFoundException;
 
-
 /**
  * Runs Smerge
  */
 public class Merger {
 	
-	public static final int BASE = 0;
-	public static final int LOCAL = 1;
-	public static final int REMOTE = 2;
     
     /**
      * @param args [BASE, LOCAL, REMOTE, MERGED] files
@@ -22,24 +18,8 @@ public class Merger {
         
         try {
             Conflict conflict = new Conflict(args[0], args[1], args[2]);
-            ASTreeMerger treeMerger = new ASTreeMerger(conflict);
-            Resolution res = treeMerger.merge();
+            Resolution res = ASTreeMerger.merge(conflict);
             res.writeFile(args[3]);
-        
-            /*
-
-            Copied from Conflerge
-    
-            TreeMerger merger = new TreeMerger(args[0], args[1], args[2]);
-            Node mergedTree = merger.merge();         
-            if (mergedTree == null) {
-                fail("Conflict encountered");
-                return;
-            }
-            writeFile(mergedTree.toString(), args[3]);
-
-
-            */
         } catch (FileNotFoundException e) {
             // fail("Files not found");
         } catch (Exception e) {
