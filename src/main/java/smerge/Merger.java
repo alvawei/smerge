22bd3d54f7cde7e9899f69bdabf9d15d02c92443
jediah6 <jediah6@cs.washington.edu>
2018-04-22 22:12:30 -0700

    added method to invoke conflerge

--- src/main/java/smerge/Merger.java
+++ src/main/java/smerge/Merger.java
@@ -2,6 +2,10 @@ package smerge;
 
 import java.io.FileNotFoundException;
 
+import com.github.javaparser.ast.Node;
+
+import conflerge.tree.TreeMerger;
+
 /**
  * Runs Smerge
  */
@@ -10,20 +14,30 @@ public class Merger {
     
     /**
      * @param args [BASE, LOCAL, REMOTE, MERGED] files
+     * @throws FileNotFoundException 
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         if (args.length != 4) {
             throw new RuntimeException("Expected args: BASE LOCAL REMOTE MERGED");
-        } 
-        
-        try {
-            Conflict conflict = new Conflict(args[0], args[1], args[2]);
-            Resolution res = ASTreeMerger.merge(conflict);
-            // res.writeFile(args[3]);
-        } catch (FileNotFoundException e) {
-            // fail("Files not found");
-        } catch (Exception e) {
-            // fail("Unexpected failure");
         }
+        
+        String base = args[0];
+        String local = args[1];
+        String remote = args[2];
+        String merged = args[3];
+        
+        
+
+        
+       String resolution = conflerge(base, local, remote);
+    }
+    
+    public static String conflerge(String base, String local, String remote) throws FileNotFoundException {        
+        TreeMerger merger = new TreeMerger(base, local, remote);
+        Node mergedTree = merger.merge();         
+        if (mergedTree == null) {
+        	return null;
+        }
+        return mergedTree.toString();
     }
 }
