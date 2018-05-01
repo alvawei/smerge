35d99b58b81f4d7d181ab3f784e789737e9a89e8
jediah6 <jediah6@cs.washington.edu>
2018-05-01 11:12:37 -0700

    added simple junit test

--- src/test/java/smerge/test/AppTest.java
+++ src/test/java/smerge/test/AppTest.java
@@ -1,4 +1,4 @@
-package com.mycompany.app;
+package smerge.test;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
---
+++ src/test/java/smerge/test/TestPythonParser.java
@@ -0,0 +1,43 @@
+package smerge.test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import smerge.ast.python.PythonParser;
+import smerge.ast.python.PythonTree;
+
+public class TestPythonParser {
+	
+	public static final String SIMPLE = "conflicts/test/test_remote.py";
+	
+	@Test
+	public void TestSimple() throws IOException {
+		File f = new File(SIMPLE);
+		String fileContent = readFile(f);
+		PythonTree tree = PythonParser.parse(f);
+		
+		assertEquals(fileContent, tree.toString());
+		
+	}
+	
+	 public static String readFile(File file) {
+		    StringBuffer stringBuffer = new StringBuffer();
+		    if (file.exists())
+		        try {
+		            //read data from file
+		            FileInputStream fileInputStream = new FileInputStream(file);
+		            int c;
+		            while ((c = fileInputStream.read()) != -1){
+		                stringBuffer.append((char) c);
+		            }
+		        } catch (IOException e) {
+		            e.printStackTrace();
+		        }
+		    return stringBuffer.toString();
+		}
+}
