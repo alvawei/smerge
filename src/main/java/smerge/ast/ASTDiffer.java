b68120548bb30ae1858c2f18ad291d596038f10b
Steven <stevem62@uw.edu>
2018-05-08 18:16:57 -0700

    trying to get started... a little stuck

--- src/main/java/smerge/ast/ASTDiffer.java
+++ src/main/java/smerge/ast/ASTDiffer.java
@@ -8,7 +8,8 @@ import java.util.Set;
 
 import smerge.ast.actions.Action;
 
-
+// given two matched trees, produce a list of actions
+// actions are things like move, add, remove nodes (lines) 
 public class ASTDiffer {
 	
 	// possible solution:
@@ -18,22 +19,22 @@ public class ASTDiffer {
 	private ASTMatcher matcher;
 	
 	public ASTDiffer(AST base, AST local, AST remote) {
-		
+
 		matcher = new ASTMatcher(base.getRoot(), local.getRoot(), remote.getRoot());
-		
-		// if trees are equal then no diff needed (probably an import conflict)
-		// gotta actually implement equals() method though
-		// or figure it out while matching
-		if (!(base.equals(local) && base.equals(remote))) {
-			
-		}
-		
+		// TODO: At this point, our matcher has gone through and identified all of the nodes (lines) that
+		// are equivalent between base/local and base/remote. The trees may look completely different,
+		// with nodes moved around, and I don't know how to deal with that. Wrote the helper methods to
+		// get the Id's but might not be necessary.
+		int baseId = matcher.getBaseId();
+		int localId = matcher.getLocalId();
+		int remoteId = matcher.getRemoteId();
 	}
-	
+
 	// merges the base->local and base->remote diffs
 	// returns null if not possible
 	public List<Action> mergedDiff() {
-		
+		List<Action> actions = new ArrayList<Action>();
+
 		return null;
 	}
 	
