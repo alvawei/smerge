54be8f68fb0714f8b21ad685308330a237643567
jediah6 <jediah6@cs.washington.edu>
2018-05-10 10:51:06 -0700

    comparator setup

--- src/main/java/smerge/ast/actions/ActionSet.java
+++ src/main/java/smerge/ast/actions/ActionSet.java
@@ -1,10 +1,14 @@
 package smerge.ast.actions;
 
 import java.util.List;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 import smerge.ast.ASTNode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 // basically a tree diff
 public class ActionSet {
@@ -14,20 +18,33 @@ public class ActionSet {
 	public List<Update> updates;
 	public List<Move> moves;
 	
+	private SortedSet<Action> actions;
+	
 	public ActionSet() {
-		this.inserts = new ArrayList<>();
-		this.deletes = new ArrayList<>();
-		this.updates = new ArrayList<>();
-		this.moves = new ArrayList<>();
+		actions = new TreeSet<Action>(new ActionSort());
 	}
 	
 	// returns true iff actions are merged into base tree
 	public boolean apply() {
+		for (Action a : actions) {
+			a.apply();
+		}
 		return false;
 	}
 	
 
 	public void add(Action a) {
+		actions.add(a);
+	}
+	
+	public class ActionSort implements Comparator<Action> {
+
+		@Override
+		public int compare(Action o1, Action o2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
 		
 	}
 }
