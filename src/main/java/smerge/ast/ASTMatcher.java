a534822c3acb31837507a609062f3099d43ec0b9
Alva Wei <alvawei@cs.washington.edu>
2018-05-09 23:25:09 -0700

    similarity score stuff

--- src/main/java/smerge/ast/ASTMatcher.java
+++ src/main/java/smerge/ast/ASTMatcher.java
@@ -36,7 +36,8 @@ public class ASTMatcher {
 	
 	private List<Match> matches;
 	
-	//private static final double SIM_THRESHOLD = 0.8;
+	// smaller similarity = good
+	private static final double SIM_THRESHOLD = 0.2;
 	//private static final double LEAF_THRESHOLD = 0.8;
 	private int localMatchId = 0;
 	private int remoteMatchId = 0;
@@ -103,7 +104,8 @@ public class ASTMatcher {
 	}
 	
 	private boolean compareLeaf(ASTNode n1, ASTNode n2) {
-		return n1.getType() == n2.getType() && n1.getLabel().equals(n2.getLabel());
+		double similarity = (double) distance(n1.getLabel(), n2.getLabel()) / Math.max(n1.getLabel().length(), n2.getLabel().length());
+		return n1.getType() == n2.getType() && similarity <= SIM_THRESHOLD;
 	}
 	
 	// in the future change to comparing nodes?
@@ -125,5 +127,26 @@ public class ASTMatcher {
 	
 	public List<Match> matches() {
 		return matches;
+	}
+	
+	// Levenshtein distance
+	private static int distance(String a, String b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+    // i == 0
+    int [] costs = new int [b.length() + 1];
+    for (int j = 0; j < costs.length; j++)
+        costs[j] = j;
+    for (int i = 1; i <= a.length(); i++) {
+        // j == 0; nw = lev(i - 1, j)
+        costs[0] = i;
+        int nw = i - 1;
+        for (int j = 1; j <= b.length(); j++) {
+            int cj = Math.min(1 + Math.min(costs[j], costs[j - 1]), a.charAt(i - 1) == b.charAt(j - 1) ? nw : nw + 1);
+            nw = costs[j];
+            costs[j] = cj;
+        }
+    }
+    return costs[b.length()];
 	}
 }
