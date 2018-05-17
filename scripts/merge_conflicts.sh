b35f62c039503c163747e3bbed1e3b3675e681b8
sung min rhee <srhee4@cs.washington.edu>
2018-05-17 15:39:22 -0700

    merge_conflict.sh change

--- scripts/merge_conflicts.sh
+++ scripts/merge_conflicts.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-if [  ]; then
+if [ $# -ne 2 ]; then
     echo "Usage: ./merge_conflicts <Repository directory> <Results directory>"
 else
     REPO_DIR=$1
     RESULTS_DIR=$2
     NUM=0
-    GET_FILES=0;
 
     touch $RESULTS_DIR/result.txt
     rm $RESULTS_DIR/result.txt
@@ -20,10 +19,11 @@ else
 	
 	git -C $REPO_DIR checkout --force -b remote $REMOTE
  	git -C $REPO_DIR checkout --force -b local $LOCAL
-	#BASE=$(git -C $REPO_DIR merge-base local remote)
 	git -C $REPO_DIR merge remote > $RESULTS_DIR/merge.txt
-	
+
+	# read the merge results line by line and find line with CONFLICT
 	while read conflict; do
+	    # handle if content CONFLICT in python file
 	    if [[ $conflict =~ .*[[:space:]]([^\.]*\.py) ]]; then
 		if [[ $conflict == *"content"* ]]; then
 		    FILEPATH=$REPO_DIR/${BASH_REMATCH[1]}
@@ -34,50 +34,30 @@ else
 			mkdir $RESULTS_DIR/conflicts
 		    fi
 		    
-		    if [ $GET_FILES -eq 1 ]; then
-			# get copies of base local remote and conflict
-			cp $FILEPATH $RESULTS_DIR/
-			mv $RESULTS_DIR/$FILENAME $RESULTS_DIR/conflicts/${NUM}_${FILENAME%.py}_conflict.py
-			
-			git -C $REPO_DIR reset --merge
-			
-			cp $FILEPATH $RESULTS_DIR/
-			mv $RESULTS_DIR/$FILENAME $RESULTS_DIR/conflicts/${NUM}_${FILENAME%.py}_local.py
-			
-			git -C $REPO_DIR checkout remote
-			
-			cp $FILEPATH $RESULTS_DIR/
-			mv $RESULTS_DIR/$FILENAME $RESULTS_DIR/conflicts/${NUM}_${FILENAME%.py}_remote.py
-			
-			git -C $REPO_DIR checkout --force -b base $BASE
-			
-			cp $FILEPATH $RESULTS_DIR/
-			mv $RESULTS_DIR/$FILENAME $RESULTS_DIR/conflicts/${NUM}_${FILENAME%.py}_base.py
-		    else
-			# try to resolve conflict using our mergetool
-			RESULT="$(yes | git -C ${REPO_DIR} mergetool --tool=smerge $FILEPATH)"
-			
-			if [[ $RESULT =~ *SUCCESS* ]]; then
-			    echo "SUCCESS" >> $RESULTS_DIR/result.txt
-
+		    # try to resolve conflict using our mergetool
+		    echo "$(yes | git -C ${REPO_DIR} mergetool --tool=smerge $FILEPATH)" > $RESULTS_DIR/mergetool.txt
+		    while read mergetool_result; do
+			if [[ $mergetool_result =~ *resolved:[[:space:]](*)/(*) ]]; then
+			    
 			    cp $FILEPATH $RESULTS_DIR/
 			    mv $RESULTS_DIR/$FILENAME $RESULTS_DIR/conflicts/${NUM}_${FILENAME%.py}_actual.py
-			 
-			else
-			    echo "FAILURE" >> $RESULTS_DIR/result.txt
-			fi
-			
-			git -C $REPO_DIR reset --merge
+			  
+			    SUCCESS=${BASH_REMATCH[1]}
+			    TOTAL=${BASH_REMATCH[2]}
 
-		    fi
+			    echo "$NUM $SUCCESS / TOTAL" >> $RESULTS_DIR/result.txt
+		    done <<< $(grep resolved $RESULTS_DIR/mergetool.txt)
 		    
-		    # get the human merged file
-		    git -C $REPO_DIR checkout --force -b merged $MERGED
-
-		    cp $FILEPATH $RESULTS_DIR/
-		    mv $RESULTS_DIR/$FILENAME $RESULTS_DIR/conflicts/${NUM}_${FILENAME%.py}_expected.py
-
+		    git -C $REPO_DIR reset --merge
+		    
 		fi
+		
+		# get the human merged file
+		git -C $REPO_DIR checkout --force -b merged $MERGED
+		
+		cp $FILEPATH $RESULTS_DIR/
+		mv $RESULTS_DIR/$FILENAME $RESULTS_DIR/conflicts/${NUM}_${FILENAME%.py}_expected.py
+		
 	    fi
 	done <<< $(grep CONFLICT $RESULTS_DIR/merge.txt)
 
