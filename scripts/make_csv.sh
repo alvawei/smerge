3a83fcc44d38de6ff90cc3419bfb5015cbfa4c6b
sung min rhee <srhee4@cs.washington.edu>
2018-05-24 18:01:39 -0700

    update script to reflect new eval metrics

--- scripts/make_csv.sh
+++ scripts/make_csv.sh
@@ -1,45 +1,29 @@
 #!/bin/bash
 
-./run_diff.sh $2/conflicts
+#./run_diff.sh $2/conflicts
 
 # The tested repository
 REPO=$1
 
 CONFLICTS=0
-RESOLVED=0
+MODIFIED=0
 UNRESOLVED=0
 
 while read line; do
     if [[ $line =~ (.*)[[:space:]](.*)/(.*) ]]; then
 	NUM=${BASH_REMATCH[1]}
-	RESOLVED=$((RESOLVED + BASH_REMATCH[2]))
+	MODIFIED=$((MODIFIED + BASH_REMATCH[2]))
 	CONFLICTS=$((CONFLICTS + BASH_REMATCH[3]))
     fi
 done < $2/result.txt
 
-UNRESOLVED=$((CONFLICTS - RESOLVED))
-
-# Number of Smerge merges identical to human merges
-PERFECT=$(grep SUCCESS $2/conflicts/*.out | wc -l)
-
-# Number of Smerge merges identical to human merges, ignoring comments
-ALL_PERFECT_NC=$(grep SUCCESS $2/conflicts/*_nc.out | wc -l)
-
-# Number of Smerge merges identical to human merges but differing in comments
-PERFECT_NC=$((ALL_PERFECT_NC-PERFECT))
-
-# Number of incorrect merges
-INCORRECT=$((RESOLVED-ALL_PERFECT_NC))
+UNRESOLVED=$((CONFLICTS - MODIFIED))
 
 # Calculate percentages
-P_CORRECT=$((PERFECT*100/CONFLICTS))
-
-P_CORRECTNC=$((PERFECT_NC*100/CONFLICTS))
+P_MODIFIED=$((MODIFIED*100/CONFLICTS))
 
 P_UNRESOLVED=$((UNRESOLVED*100/CONFLICTS))
 
-P_INCORRECT=$((INCORRECT*100/CONFLICTS))
 
-
-# echo "Repo,Conflicts,Unresolved,Correct,CorrectNC,Incorrect,%Correct,%CorrectCW,%Unresolved,%Incorrect" > ${REPO}.csv
-printf "%s,%d,%d,%d,%d,%d,%d,%d,%d,%d\n" "$REPO" "$CONFLICTS" "$UNRESOLVED" "$PERFECT" "$PERFECT_NC" "$INCORRECT" "$P_CORRECT" "$P_CORRECTNC" "$P_UNRESOLVED" "$P_INCORRECT" > ${REPO}.csv
+echo "Repo,Conflicts,Modified,Unresolved,%Modified,%Unresolved" > ${REPO}.csv
+printf "%s,%d,%d,%d,%d,%d\n" "$REPO" "$CONFLICTS" "$MODIFIED" "$UNRESOLVED" "$P_MODIFIED" "$P_UNRESOLVED" >> ${REPO}.csv
--- scripts/test.sh
+++ scripts/test.sh
@@ -4,6 +4,9 @@ if [ ! -d test_results ]; then
     mkdir test_results
 fi
 
+# create a final table with all of the csv's included
+echo "Repo,Conflicts,Modified,Unresolved,%Modified,%Unresolved" > table.csv
+
 cat repos.txt | while read line; do
     REPO_NAME=$(echo $line | cut -d '/' -f 5)
     CUR_DIR=$(pwd)
@@ -11,9 +14,5 @@ cat repos.txt | while read line; do
     git clone $line.git
     cd $CUR_DIR
     ./test_repo.sh $REPO_NAME
-    
+    sed '2q;d' ${REPO_NAME}.csv >> table.csv
 done
-
-# create a final table with all of the csv's included
-#echo "Repo,Conflicts,Unresolved,Correct,CorrectNC,Incorrect,%Correct,%CorrectCW,%Unresolved,%Incorrect" > table.csv
-#cat *.csv >> table.csv
